c75a09387a7fdbca1fd223da6c88c57939cbe085
kapkaaa <kafkasanjaya123360@gmail.com>
2025-06-29 01:02:50 +0700

    adding dashboard

---
+++ dashboard.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Manajemen_kelas
+{
+    partial class dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.day = new System.Windows.Forms.Label();
+            this.time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dashboard";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 53);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.day);
+            this.panel2.Controls.Add(this.time);
+            this.panel2.Location = new System.Drawing.Point(656, 8);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(132, 83);
+            this.panel2.TabIndex = 0;
+            // 
+            // day
+            // 
+            this.day.AutoSize = true;
+            this.day.ForeColor = System.Drawing.SystemColors.Control;
+            this.day.Location = new System.Drawing.Point(19, 22);
+            this.day.Name = "day";
+            this.day.Size = new System.Drawing.Size(0, 13);
+            this.day.TabIndex = 3;
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.ForeColor = System.Drawing.SystemColors.Control;
+            this.time.Location = new System.Drawing.Point(50, 48);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(0, 13);
+            this.time.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Location = new System.Drawing.Point(35, 120);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(735, 287);
+            this.panel3.TabIndex = 1;
+            // 
+            // dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "dashboard";
+            this.Text = "dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.dashboard_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label day;
+        internal System.Windows.Forms.Label time;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
---
+++ dashboard.cs
@@ -0,0 +1,113 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manajemen_kelas
+{
+    public partial class dashboard : Form
+    {
+        public dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time.Text = DateTime.Now.ToString("HH:mm:ss");
+            day.Text = DateTime.Now.ToString("dddd, dd MMMM yyyy", new CultureInfo("id-ID"));
+        }
+
+        private void dashboard_Load(object sender, EventArgs e)
+        {
+            int role = Session.CurrentRole;
+
+            rolesForm();
+                //if (role == 1)
+                //{
+                //    LoadFormToPanel(new walas());
+                //}
+                //elseif (role  == 3) {
+                //    LoadFormToPanel (new sekretaris());
+                //}
+                //elseif (role == 4) {
+                //    LoadFormToPanel(new bendahara());
+                //}
+                //elseif (role == 5) {
+                //    LoadFormToPanel(new siswa());
+                //}
+
+
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void rolesForm()
+        {
+            string form = "";
+
+            koneksi k = new koneksi();
+            k.sql();
+            MySqlConnection conn = k.get();
+
+            try
+            {
+
+                using (MySqlCommand cmd = new MySqlCommand("select * from roles where role_id = @role", conn))
+                {
+                    cmd.Parameters.AddWithValue("@role", Session.CurrentRole);
+                    using (MySqlDataReader  reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            form = reader["form_name"].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Role Tidak ditemukan");
+                            return;
+                        }
+                    }
+                }
+
+                Type formtype = Type.GetType("Manajemen_kelas." + form);
+                if (formtype != null)
+                {
+                    Form formins =  (Form)Activator.CreateInstance(formtype);
+                    LoadFormToPanel(formins);
+                }
+                else
+                {
+                    MessageBox.Show("Form Tidak ditemukan");
+                }
+            } 
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error " + ex.Message);
+            }
+            finally
+            {
+                k.close();
+            }
+        }
+
+        private void LoadFormToPanel(Form childForm)
+        {
+            panel3.Controls.Clear();
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+
+            panel3.Controls.Add(childForm);
+            childForm.Show();
+        }
+
+    }
+}
