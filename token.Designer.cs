9c2228a24f4bb921352da87966510a65a85fe066
kapkaaa <kafkasanjaya123360@gmail.com>
2025-06-29 00:57:11 +0700

    adding form validation token

---
+++ token.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Manajemen_kelas
+{
+    partial class token
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TOKEN";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Label2.Location = new System.Drawing.Point(26, 98);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(229, 39);
+            this.Label2.TabIndex = 4;
+            this.Label2.Text = "*Token berhasil dikirim.Mohon cek Gmail Anda\r\n dan masukkan token pada form yang " +
+    "tersedia.\r\n\r\n";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(33, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 26);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Masukkan Token";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(98)))), ((int)(((byte)(204)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(33, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 31);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(40, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "*Kirim ulang code";
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(323, 72);
+            this.panel1.TabIndex = 9;
+            // 
+            // token
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(323, 339);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "token";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "token";
+            this.Load += new System.EventHandler(this.token_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label Label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        internal System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ token.cs
@@ -0,0 +1,125 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Manajemen_kelas
+{
+    public partial class token : Form
+    {
+        public token()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Enabled = false;
+
+            // Kirim ulang token ke email
+            //email mail = new email();
+            //mail.SendEmail(emailTujuan, token, username);
+
+            // Tampilkan notifikasi
+            MessageBox.Show("Token berhasil dikirim ulang. Silakan cek email Anda.", "Kirim Ulang Token", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Mulai timer cooldown
+            timer1.Interval = 30000; // 30 detik cooldown
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Enabled = true;
+            timer1.Stop();
+        }
+
+        private void token_Load(object sender, EventArgs e)
+        {
+            int RoleId = Session.CurrentRole;
+
+            BuatRoundedButton(button1);
+        }
+
+        private void BuatRoundedButton(Button btn)
+        {
+            int borderRadius = btn.Height / 2;
+
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90);
+            path.AddArc(btn.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90);
+            path.AddArc(btn.Width - borderRadius * 2, btn.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90);
+            path.AddArc(0, btn.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90);
+            path.CloseFigure();
+
+            btn.Region = new Region(path);
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Masukkan Token")
+            {
+                textBox1.Text = "";
+
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Masukkan Token";
+
+                textBox1.ForeColor = Color.Silver;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            koneksi k = new koneksi();
+            k.sql();
+            MySqlConnection conn = k.get();
+
+
+            using (MySqlCommand cmd = new MySqlCommand("SELECT * from auth_tokens where token = @token", conn))
+            {
+                cmd.Parameters.AddWithValue("@token", textBox1.Text);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        DateTime expiredAt = Convert.ToDateTime(reader["expired_at"]);
+
+                        if (DateTime.Now > expiredAt)
+                        {
+                            MessageBox.Show("Token sudah expired.");
+                        }
+                        else
+                        {
+                            dashboard home = new dashboard();
+                            home.Show();
+                            this.Close();
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Token tidak valid.", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        k.close();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
