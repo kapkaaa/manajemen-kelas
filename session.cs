c84543e31c4d2b48b23644d41668aff5f315ef3e
kapkaaa <kafkasanjaya123360@gmail.com>
2025-06-29 00:59:15 +0700

    adding session

---
+++ session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manajemen_kelas
+{
+    internal class session
+    {
+    }
+    public static class Session
+    {
+        public static int CurrentUserID { get; set; }
+        public static string CurrentUserName { get; set; }
+        public static int CurrentRole { get; set; }
+        public static bool IsLoggedIn { get; set; }
+    }
+
+}
