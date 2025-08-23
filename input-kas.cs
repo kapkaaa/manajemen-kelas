5645f663de4306229a17e16e2e23d7320549ea2d
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2025-08-23 14:37:36 +0700

    form input kas

---
+++ input-kas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manajemen_kelas
+{
+    public partial class input_kas : Form
+    {
+        public input_kas()
+        {
+            InitializeComponent();
+        }
+    }
+}
