b164bb85f7a31f6ed70526e54a16c280cf392175
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2025-08-25 10:18:25 +0700

    adding function clear session

--- session.cs
+++ session.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Manajemen_kelas
 {
@@ -15,6 +17,14 @@ namespace Manajemen_kelas
         public static string CurrentUserName { get; set; }
         public static int CurrentRole { get; set; }
         public static bool IsLoggedIn { get; set; }
+
+        public static void Clear()
+        {
+            CurrentUserID = 0;
+            CurrentUserName = null;
+            CurrentRole = 0;
+            IsLoggedIn = false;
+        }
     }
 
 }
