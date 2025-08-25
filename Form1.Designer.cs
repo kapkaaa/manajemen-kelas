0b0f45e0f408db7d6837733a6d1a9eb3ee5da082
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2025-08-25 10:15:14 +0700

    fix move form login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Manajemen_kelas
 {
-    partial class Login
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,104 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.panel1.BackColor = System.Drawing.Color.MediumOrchid;
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
             this.panel1.Location = new System.Drawing.Point(435, 139);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(521, 525);
             this.panel1.TabIndex = 1;
             // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBox1.Location = new System.Drawing.Point(114, 210);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(293, 26);
-            this.textBox1.TabIndex = 5;
-            this.textBox1.Text = "Masukkan Nama Anda";
-            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
-            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.BackColor = System.Drawing.Color.Red;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(114, 316);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(293, 31);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Exit";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(237, 27);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(47, 48);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(98)))), ((int)(((byte)(204)))));
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(114, 271);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(293, 31);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Next";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 160);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(203, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Masuk ke aplikasi";
-            // 
             // Login
             // 
-            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
@@ -136,9 +52,6 @@
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Login_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -146,11 +59,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,123 +15,30 @@ using System.Windows.Forms;
 
 namespace Manajemen_kelas
 {
-    public partial class Login : Form
+    public partial class Form1 : Form
     {
-        public Login()
+        public Form1()
         {
             InitializeComponent();
+
+            panelform(new login(this));
+        }
+
+        public void panelform(Form form)
+        {
+            panel1.Controls.Clear();
+
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+
+            panel1.Controls.Add(form);
+            form.Show();
         }
 
         private void Login_Load(object sender, EventArgs e)
         {
 
-
-
-            BuatRoundedButton(button1);
-            BuatRoundedButton(button2);
-        }
-
-        private void BuatRoundedButton(Button btn)
-        {
-            int borderRadius = btn.Height / 2;
-
-            GraphicsPath path = new GraphicsPath();
-            path.AddArc(0, 0, borderRadius * 2, borderRadius * 2, 180, 90);
-            path.AddArc(btn.Width - borderRadius * 2, 0, borderRadius * 2, borderRadius * 2, 270, 90);
-            path.AddArc(btn.Width - borderRadius * 2, btn.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 0, 90);
-            path.AddArc(0, btn.Height - borderRadius * 2, borderRadius * 2, borderRadius * 2, 90, 90);
-            path.CloseFigure();
-
-            btn.Region = new Region(path);
-        }
-
-        private void textBox1_Enter(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "Masukkan Nama Anda")
-            {
-                textBox1.Text = "";
-
-                textBox1.ForeColor = Color.Black;
-            }
-        }
-
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "")
-            {
-                textBox1.Text = "Masukkan Nama Anda";
-
-                textBox1.ForeColor = Color.Silver;
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            string username = textBox1.Text;
-            string emailTujuan = "";
-            string userId = "";
-            koneksi k = new koneksi();
-            k.sql();
-            MySqlConnection conn = k.get();
-
-            // Cari email berdasarkan username
-            using (MySqlCommand cmd = new MySqlCommand("SELECT gmail, id, role_id FROM users WHERE nama = @username", conn))
-            {
-                cmd.Parameters.AddWithValue("@username", username);
-                using (MySqlDataReader reader = cmd.ExecuteReader())
-                {
-                    if (reader.HasRows)
-                    {
-                        reader.Read();
-                        emailTujuan = reader["gmail"].ToString();
-                        userId = reader["id"].ToString();
-                        Session.CurrentUserID = Convert.ToInt32(reader["id"]);
-                        Session.CurrentUserName = username;
-                        Session.CurrentRole = Convert.ToInt32(reader["role_id"]);
-                        Session.IsLoggedIn = true;
-                    }
-                    else
-                    {
-                        MessageBox.Show("Username tidak ditemukan.", "Gagal", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        k.close();
-                        return;
-                    }
-                }
-            }
-
-            // Generate token
-            Random rand = new Random();
-            string tokenn = rand.Next(100000, 999999).ToString(); // Token 6 digit
-                                                                 // Waktu token
-            DateTime createdAt = DateTime.Now;
-            DateTime expiredAt = createdAt.AddMinutes(5); // Token berlaku 5 menit
-
-
-            // Simpan token ke database
-            using (MySqlCommand cmd = new MySqlCommand("insert into auth_tokens (user_id, token, created_at, expired_at) values (@id, @token, @created_at, @expired_at)", conn))
-            {
-                cmd.Parameters.AddWithValue("@token", tokenn);
-                cmd.Parameters.AddWithValue("@username", username);
-                cmd.Parameters.AddWithValue("@id", userId);
-                cmd.Parameters.AddWithValue("@created_at", createdAt.ToString("yyyy-MM-dd HH:mm:ss"));
-                cmd.Parameters.AddWithValue("@expired_at", expiredAt.ToString("yyyy-MM-dd HH:mm:ss"));
-                cmd.ExecuteNonQuery();
-            }
-
-            // Kirim token ke email yang sudah ditemukan
-            //email mail = new email();
-            //mail.SendEmail(emailTujuan, tokenn, username);
-            //MessageBox.Show("berhasil");
-            token tkn = new token();
-            tkn.Show();
-            panel1.Visible = false;
-
-            k.close();
         }
 
     }
