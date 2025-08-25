3bc0ec3e13206c73365135b43ec8d5ec04c85c09
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2025-08-25 10:34:14 +0700

    report kas

---
+++ laporkas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manajemen_kelas
+{
+    public partial class laporkas : Form
+    {
+        public laporkas()
+        {
+            InitializeComponent();
+        }
+    }
+}
