f2336513a8d454a04c2c3eabb750895fa41a92d3
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2025-08-30 17:37:01 +0700

    adding pengeluaran kas

--- pengeluaran_kas.Designer.cs
+++ pengeluaran_kas.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // pengeluaran_kas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "pengeluaran_kas";
             this.Text = "pengeluaran_kas";
+            this.Load += new System.EventHandler(this.pengeluaran_kas_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
