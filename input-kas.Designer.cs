7dabfdb364bdbfef6e36e2ede7c188ca503c4369
Kafka Ahmad Sanjaya <kafkaasanjaya@gmail.com>
2025-09-01 10:45:03 +0700

    fix design and adding clear button

--- input-kas.Designer.cs
+++ input-kas.Designer.cs
@@ -30,23 +30,25 @@
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Kas = new System.Windows.Forms.ListBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.cmbsmt = new System.Windows.Forms.ComboBox();
+            this.tbktr = new System.Windows.Forms.TextBox();
+            this.tbjl = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbnm = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbkd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -64,13 +66,42 @@
             // 
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.Kas);
             this.groupBox1.Location = new System.Drawing.Point(468, 96);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(320, 342);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Search kas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Belum di Bayar";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Semester 5",
+            "Semester 6"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Semester";
+            // 
+            // Kas
+            // 
+            this.Kas.FormattingEnabled = true;
+            this.Kas.Location = new System.Drawing.Point(0, 100);
+            this.Kas.Name = "Kas";
+            this.Kas.Size = new System.Drawing.Size(320, 238);
+            this.Kas.TabIndex = 0;
             // 
             // textBox1
             // 
@@ -87,88 +118,91 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Controls.Add(this.dtp);
-            this.groupBox2.Controls.Add(this.comboBox2);
-            this.groupBox2.Controls.Add(this.textBox5);
-            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.cmbsmt);
+            this.groupBox2.Controls.Add(this.tbktr);
+            this.groupBox2.Controls.Add(this.tbjl);
             this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label5);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.tbnm);
             this.groupBox2.Controls.Add(this.label3);
-            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.tbkd);
             this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Location = new System.Drawing.Point(29, 466);
+            this.groupBox2.Location = new System.Drawing.Point(794, 51);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(731, 225);
+            this.groupBox2.Size = new System.Drawing.Size(348, 387);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Bayar Kas";
             // 
-            // listBox1
+            // dtp
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(0, 100);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(320, 238);
-            this.listBox1.TabIndex = 0;
+            this.dtp.Location = new System.Drawing.Point(115, 163);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(208, 20);
+            this.dtp.TabIndex = 10;
             // 
-            // comboBox1
+            // cmbsmt
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cmbsmt.FormattingEnabled = true;
+            this.cmbsmt.Items.AddRange(new object[] {
             "Semester 5",
             "Semester 6"});
-            this.comboBox1.Location = new System.Drawing.Point(6, 45);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.Text = "Semester";
+            this.cmbsmt.Location = new System.Drawing.Point(115, 208);
+            this.cmbsmt.Name = "cmbsmt";
+            this.cmbsmt.Size = new System.Drawing.Size(208, 21);
+            this.cmbsmt.TabIndex = 3;
+            this.cmbsmt.Text = "Semester 5";
             // 
-            // label1
+            // tbktr
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 81);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Belum di Bayar";
+            this.tbktr.Location = new System.Drawing.Point(115, 246);
+            this.tbktr.Multiline = true;
+            this.tbktr.Name = "tbktr";
+            this.tbktr.Size = new System.Drawing.Size(208, 62);
+            this.tbktr.TabIndex = 9;
             // 
-            // label2
+            // tbjl
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(16, 54);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 16);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Kode :";
+            this.tbjl.Location = new System.Drawing.Point(115, 131);
+            this.tbjl.Name = "tbjl";
+            this.tbjl.ReadOnly = true;
+            this.tbjl.Size = new System.Drawing.Size(208, 20);
+            this.tbjl.TabIndex = 8;
             // 
-            // textBox2
+            // label7
             // 
-            this.textBox2.Location = new System.Drawing.Point(85, 53);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(267, 20);
-            this.textBox2.TabIndex = 1;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(16, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 16);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Tanggal :";
             // 
-            // label3
+            // label6
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(16, 132);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Jumlah :";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(16, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Semester :";
             // 
-            // textBox3
+            // label5
             // 
-            this.textBox3.Location = new System.Drawing.Point(85, 94);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(267, 20);
-            this.textBox3.TabIndex = 3;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Keterangan :";
             // 
             // label4
             // 
@@ -180,77 +214,66 @@
             this.label4.TabIndex = 4;
             this.label4.Text = "Nama :";
             // 
-            // label5
+            // tbnm
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(396, 135);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(93, 16);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Keterangan :";
+            this.tbnm.Location = new System.Drawing.Point(115, 94);
+            this.tbnm.Name = "tbnm";
+            this.tbnm.Size = new System.Drawing.Size(208, 20);
+            this.tbnm.TabIndex = 3;
             // 
-            // label6
+            // label3
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(396, 98);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(80, 16);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "Semester :";
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Jumlah :";
             // 
-            // label7
+            // tbkd
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(396, 57);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(68, 16);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Tanggal :";
+            this.tbkd.Location = new System.Drawing.Point(115, 53);
+            this.tbkd.Name = "tbkd";
+            this.tbkd.ReadOnly = true;
+            this.tbkd.Size = new System.Drawing.Size(208, 20);
+            this.tbkd.TabIndex = 1;
             // 
-            // textBox4
+            // label2
             // 
-            this.textBox4.Location = new System.Drawing.Point(85, 131);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(267, 20);
-            this.textBox4.TabIndex = 8;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Kode :";
             // 
-            // textBox5
+            // button1
             // 
-            this.textBox5.Location = new System.Drawing.Point(495, 135);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(208, 62);
-            this.textBox5.TabIndex = 9;
+            this.button1.Location = new System.Drawing.Point(19, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 32);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Simpan";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // comboBox2
+            // button2
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "Semester 5",
-            "Semester 6"});
-            this.comboBox2.Location = new System.Drawing.Point(495, 97);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 3;
-            this.comboBox2.Text = "Semester";
-            // 
-            // dtp
-            // 
-            this.dtp.Location = new System.Drawing.Point(495, 52);
-            this.dtp.Name = "dtp";
-            this.dtp.Size = new System.Drawing.Size(208, 20);
-            this.dtp.TabIndex = 10;
+            this.button2.Location = new System.Drawing.Point(218, 338);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 32);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Bersih";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // input_kas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(165)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(808, 807);
+            this.ClientSize = new System.Drawing.Size(1217, 749);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.groupBox1);
@@ -274,20 +297,22 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox Kas;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbkd;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox tbnm;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox tbjl;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox cmbsmt;
+        private System.Windows.Forms.TextBox tbktr;
         private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- input-kas.cs
+++ input-kas.cs
@@ -34,6 +34,7 @@ namespace Manajemen_kelas
             // atau jika ingin ada hari & bulan dalam bahasa Indonesia
             dtp.CustomFormat = "dddd, dd MMMM yyyy";
 
+            data();
         }
 
         private void textBox1_Enter(object sender, EventArgs e)
@@ -58,7 +59,16 @@ namespace Manajemen_kelas
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            search(textBox1.Text);
+            string keyword = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(keyword))
+            {
+                data();
+            }
+            else
+            {
+                search(keyword);
+            }
         }
 
         private void search(string keyword)
@@ -70,7 +80,7 @@ namespace Manajemen_kelas
             {
                 MySqlConnection conn = k.get();
 
-                using (MySqlCommand cmd = new MySqlCommand("select * from pending_kas_siswa p join users u ON p.user_id = u.id where u.nama LIKE @keyword"))
+                using (MySqlCommand cmd = new MySqlCommand("select * from kas_siswa p join users u ON p.user_id = u.id where u.nama LIKE @keyword"))
                 {
                     cmd.Parameters.AddWithValue("keyword", "%" + keyword + "%");
                     MySqlDataAdapter da = new MySqlDataAdapter();
@@ -98,10 +108,24 @@ namespace Manajemen_kelas
             {
                 MySqlConnection conn = k.get();
 
-                using (MySqlCommand cmd = new MySqlCommand("select * from kas_siswa ks join users u on p.user_id = u.id "))
+                using (MySqlCommand cmd = new MySqlCommand("select u.nama, ks.*, s.semester from kas_siswa ks join users u on ks.user_id = u.id join semester s on ks.semester_id = s.id", conn))
                 {
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
 
+                    dataGridView1.DataSource= dt;
+                    dataGridView1.Columns["id"].Visible = false;
+                    dataGridView1.Columns["user_id"].Visible = false;
+
+                    dataGridView1.Columns["kode"].HeaderText = "Kode";
+                    dataGridView1.Columns["nama"].HeaderText = "Nama Siswa";
+                    dataGridView1.Columns["jumlah"].HeaderText = "Kas";
+                    dataGridView1.Columns["tanggal"].HeaderText = "Tanggal bayar";
+                    dataGridView1.Columns["semester"].HeaderText = "Semester";
+                    dataGridView1.Columns["keterangan"].HeaderText = "Keterangan";
                 }
+                
             } catch (Exception ex)
             {
                 MessageBox.Show("error" + ex.Message);
@@ -110,6 +134,23 @@ namespace Manajemen_kelas
             { 
                 k.close(); 
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void clear()
+        {
+            tbjl.Clear();
+            tbkd.Clear();
+            tbktr.Clear();
+            tbnm.Clear();
+            cmbsmt.SelectedIndex = -1;
+            dtp.Value = DateTime.Now;
+            data();
+            textBox1.Clear();
         }
     }
 }
